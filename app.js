34490cd5bbb0d0c6980c23372e7349abbf47e26e
Camilla <camilla.taina.santos@accenture.com>
2022-03-29 12:36:39 -0300

    finalização da atividade

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function vetorI() {
     const numero = document.getElementById("numero").value;
     console.log(numero);
     
-    numerosArray.push(numero);
+    numerosArray.unshift(numero);
     console.log(numerosArray);
     document.getElementById("numero").value="";
 }
@@ -22,15 +22,28 @@ function vetorF() {
 function resetar() {
     numerosArray.splice(0, numerosArray.length);
     console.log(numerosArray);
+    document.getElementById("numero").value="";
 }
 
 function indice() {
     const indiceA = document.getElementById("indice").value;
-   const fila =  numerosArray.splice(indiceA, 1);
+     numerosArray.splice(indiceA, 1);
     console.log(numerosArray);
-    if(indiceA < numerosArray.length) {
+    document.getElementById("indice").value="";
+    if(indiceA <= numerosArray.length) {
         console.log("ok");
     }else {
         alert("Indice Invalido!");
     }
+    const lista = document.getElementById("lista");
+    numerosArray.sort((a,b) => a-b);
+
+    for(const element of numerosArray) {
+        lista.innerHTML += `<li>${element}</li>`;
+        console.log(element);
+    }
 }
+
+
+ 
+
